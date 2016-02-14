bac12fede9918267bbd5c8b6c9e0c1bedfa21a11
Wei Sing Ta <james.mma@gmail.com>
2016-02-13 19:02:20 -0500

    Add basic hibernate config

---
+++ src/java/mwm/Startup.java
@@ -0,0 +1,32 @@
+package mwm;
+import org.hibernate.*;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.mysql.jdbc.Driver;
+
+public class Startup {
+	private SessionFactory sessionFactory;
+	
+	protected void setUp() throws Exception {
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure() // configures settings from hibernate.cfg.xml
+				.build();
+		try {
+			sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+		}
+		catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+			// so destroy it manually.
+			StandardServiceRegistryBuilder.destroy( registry );
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
